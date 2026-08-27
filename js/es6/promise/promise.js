da3f1a6a96add664c05ec46a90143cd44482b9cd
JackieZhou917 <zjj030917@gmail.com>
2026-08-27 18:57:29 +0800

    'promise'

--- js/es6/promise/promise.js
+++ js/es6/promise/promise.js
@@ -1,10 +1,10 @@
 function Promise(func) {
-    this.state = 'pending'   //准备状态
-    this.arr = [foo]
+    this.state = 'pending'   // 准备状态
+    this.arr = [foo]         // 用来存then里的那个箭头函数
 
     const resolve = () => {
         this.state = 'resolved'   //成功状态
-        // foo()
+        // foo() 触发then里的箭头函数
     }
     const reject = () => {
         this.state = 'rejected'   //失败状态
@@ -14,5 +14,6 @@ function Promise(func) {
 }
 
 new Promise((resolve, reject) => {
+    // 自己要运行的函数()
     resolve()
 })
